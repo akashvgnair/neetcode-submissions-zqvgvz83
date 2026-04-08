1c4cdd9695022a68c2ba537c52624d3d6a8f8764
akashvgnair <akashvgnair@gmail.com>
2026-04-08 21:10:27 +0200

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char item: s.toCharArray()) {
+            if(!map.containsKey(item)) {
+                map.put(item, 1);
+            } else {
+                map.put(item, map.get(item) + 1);
+            }
+        }
+
+        for (char item: t.toCharArray()) {
+            if(map.containsKey(item)) {
+                if(map.get(item) == 1) {
+                    map.remove(item);
+                } else {
+                    map.put(item, map.get(item) - 1);
+                }   
+            } else {
+                return false;
+            }
+        }
+
+        if(map.isEmpty()) {
+            return true;
+        }
+
+        return false;
+
+    }
+}
